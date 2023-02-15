f24e3f46522e864057e41c3bc8d8036db4b823f1
Mostafa Nezar <118356129+mostafanezar@users.noreply.github.com>
2023-02-15 21:55:02 +0200

    Add files via upload

---
+++ Java/FileREader.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.mysixthproject;
+
+import java.io.File;
+
+/**
+ *
+ * @author DAR-M
+ */
+public class FileREader extends File {
+
+    public FileREader(File file) {
+    }
+    
+}
